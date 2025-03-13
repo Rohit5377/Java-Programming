e235c16b17141f6fce6c1ea30d28017ddb8027ff
Rohit Kumar <166222791+Rohit5377@users.noreply.github.com>
2025-03-13 18:10:00 +0530

    Add files via upload

---
+++ CountSetBit.java
@@ -0,0 +1,11 @@
+public class CountSetBit {
+    public static void main(String[] args) {
+        int n=5;
+        int count=0;
+        while(n>0){
+            n=n&(n-1);
+            count++;
+        }
+        System.out.println(count);
+    }
+}
