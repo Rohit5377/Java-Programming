7770972402e0ca5f555d07b94d25e991a572f77f
Rohit Kumar <166222791+Rohit5377@users.noreply.github.com>
2025-05-27 07:30:56 +0530

    Add files via upload

---
+++ 21.PostfixToInfix.java
@@ -0,0 +1,20 @@
+import java.util.Stack;
+
+class PostfixToInfix {
+    public static void main(String[] args) {
+        String str="953+4*8/-";
+        Stack<String> val=new Stack<>();
+        for(int i=0;i<str.length();i++){
+            char ch=str.charAt(i);
+            int n=str.charAt(i)-'0';
+            if(n>=0 && n<=9){
+                val.push(""+ch);
+            }else{
+                String v2=val.pop();
+                String v1=val.pop();
+                val.push('('+v1+ch+v2+')');
+            }
+        }
+        System.out.println(val.peek());
+    }
+}
