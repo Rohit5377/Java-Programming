add8a9356166d652238b3d7f4a40e42a4b35d494
Rohit Kumar <166222791+Rohit5377@users.noreply.github.com>
2025-06-02 07:08:54 +0530

    Add files via upload

---
+++ 5.Diameter.java
@@ -0,0 +1,32 @@
+class Diameter {
+
+      public class TreeNode {
+          int val;
+          TreeNode left;
+          TreeNode right;
+          TreeNode() {}
+          TreeNode(int val) { this.val = val; }
+          TreeNode(int val, TreeNode left, TreeNode right) {
+              this.val = val;
+              this.left = left;
+              this.right = right;
+          }
+      }
+
+    class Solution {
+        public int diameterOfBinaryTree(TreeNode root) {
+            if(root==null) return 0;
+            int l=diameterOfBinaryTree(root.left);
+            int r=diameterOfBinaryTree(root.right);
+            int mid=height(root.left)+height(root.right);
+            if(root.left!=null) mid++;
+            if(root.right!=null) mid++;
+            return Math.max(mid,Math.max(l,r));
+        }
+
+        public int height(TreeNode root){
+            if(root==null || (root.left==null && root.right==null)) return 0;
+            return 1+Math.max(height(root.left),height(root.right));
+        }
+    }
+}
